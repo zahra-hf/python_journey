96942a53783830b7d975e2cdde6c1e48807695c7
zahra-hf <z.hosseini745@gmail.com>
2026-06-04 15:40:07 +0330

    add list exercise

---
+++ 03_list.py
@@ -0,0 +1,6 @@
+#Write code that removes the number 3 from the given list and adds the number 10 to the end of the list.
+
+my_list = [1, 2, 3, 4, 5]
+my_list.remove(3)
+my_list.append(10)
+print(my_list)
