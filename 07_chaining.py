9ed57b4928dd336b970a8a35522a7a4581b7a84f
zahra-hf <z.hosseini745@gmail.com>
2026-06-04 15:50:44 +0330

    add chaining methods exercise

---
+++ 07_chaining.py
@@ -0,0 +1,6 @@
+#Write a program that receives three input numbers and checks whether the first number is less than the second number and the second number is less than or equal to the third number.
+
+a = int(input())
+b = int(input())
+c = int(input())
+print(a < b <= c)
