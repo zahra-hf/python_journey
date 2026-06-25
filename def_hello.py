fd5a35110f595d83ee03ced8205f8f2b08ab0bce
zahra-hf <z.hosseini745@gmail.com>
2026-06-25 15:09:23 +0330

    use def for greeting

---
+++ def_hello.py
@@ -0,0 +1,9 @@
+def hello(name):
+    '''
+    This function says hello to name,
+    to the length of name.
+    '''
+    for i in range(len(name)):
+        print(f"oh hello {name}")
+    
+hello("rose")
