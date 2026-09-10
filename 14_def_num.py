4fc8dfee085285add0fd301e1e97bce706a4ce50
zahra-hf <z.hosseini745@gmail.com>
2026-09-10 14:21:35 +0330

    def numbers exercise

---
+++ 14_def_num.py
@@ -0,0 +1,8 @@
+#Write a function called is_positive that takes a number and returns True if the number is positive or zero.
+#False otherwise.
+
+def is_positive(nem):
+    return num >= 0
+
+num = int(input())
+print(is_positive(num))
