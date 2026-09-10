656e530560a190e756e01a852b17626bed8c6163
zahra-hf <z.hosseini745@gmail.com>
2026-09-10 14:22:08 +0330

    def sum exercise

---
+++ 15_def_sum.py
@@ -0,0 +1,9 @@
+#A function that takes two integers and calculates the sum of their squares.
+
+def sum_of_squeres(a, b):
+    return a ** 2 + b ** 2
+
+a = int(input())
+b = int(input())
+
+print(sum_of_squeres(a, b))
