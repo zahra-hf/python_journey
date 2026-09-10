b47ba16560ef97c7d655fca30440669387cd1bc9
zahra-hf <z.hosseini745@gmail.com>
2026-09-10 14:18:36 +0330

    Delete def_hello.py

--- def_hello.py
+++
@@ -1,9 +0,0 @@
-def hello(name):
-    '''
-    This function says hello to name,
-    to the length of name.
-    '''
-    for i in range(len(name)):
-        print(f"oh hello {name}")
-    
-hello("rose")
